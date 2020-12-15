37c3196a75b8dc5f1e22ff4ab7a7f32e15f9ff80
Bromaster369 <chrispardopersonal@gmail.com>
2020-12-15 14:38:07 -0500

    AModified views and actions. Added local message.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
     get '/signup' do
         if !logged_in?
-          erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+          erb :'users/create_user', locals: {message: "Please create a new account before you sign in"}
         else
           redirect to '/reports'
         end
