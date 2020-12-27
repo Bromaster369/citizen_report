6b548984bd685360607fc90816a4e8128926a44e
Bromaster369 <chrispardopersonal@gmail.com>
2020-12-27 18:05:42 -0500

    added a way to show edit option if that report belongs to the current user or display the username of whoever made that report

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,8 @@ class ApplicationController < Sinatra::Base
       end 
 
       def current_user
-       User.find_by_id(session[:user_id])
+       User.find_by_id(session[:user_id]) 
       end 
     end 
 end
+
